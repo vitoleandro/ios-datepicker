2b01bd6abe814c837b9eef70aa403015fea18f83
Leandro Vitor <vitoleandro@gmail.com>
2021-01-21 15:02:04 -0300

    Initial Commit

---
+++ DatePicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DatePicker
+//
+//  Created by Leandro Vitor on 21/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
